356dab16acf3e57d39425159cf633102b62b9931
Pascu Ioana-Georgia <ioana.pascu@history.bucharest.university>
2024-01-30 15:34:33 +0200

    input outlines for success, error and blur implemented

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ let questionButtons = document.getElementsByClassName("questionButton");
 let questionAnswers = document.getElementsByClassName("FAQAnswer");
 let formErrorMessage = document.getElementsByClassName("formErrorText")[0];
 let formButton = document.getElementById("formButton");
+let contactInputEmailWrapper = document.getElementById("contactInputEmail");  
+
+// error and success styling removed on click outside the input for user accommodation
+function focusOutMail(){
+    formErrorMessage.style.display = 'none'
+    contactInputEmailWrapper.style.outline ='none'
+}
 
 function validateForm(event) {
-
   let contactInputEmail = document.getElementById("contactInputEmail").value;
   let contactInputEmailTarget = event.target;
 //   simple email typechecking
@@ -15,9 +21,11 @@ function validateForm(event) {
   if (event.key === "Enter" || event.keyCode === 13) {
     if (!contactInputEmail.includes("@")) {
       formErrorMessage.style.display = "block";
+      contactInputEmailWrapper.style.outline = "2px solid #eb5757";
       formButton.classList.add("formDisabledButton");
     } else {
       formErrorMessage.style.display = "none";
+      contactInputEmailWrapper.style.outline = "2px solid #0eac00";
       formButton.classList.remove("formDisabledButton");
     }
   }
